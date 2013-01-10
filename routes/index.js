1768c4f1fbd40a1615fe1198a12cf4eeff2d2323
paulallies <paul.allies@gmail.com>
2013-01-10 12:15:16 +0200

    added edit

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,12 @@ exports.registerRoutes = function(app){
     app.get('/account/register', accountRoutes.register);
     app.get('/helloworld', helloworldRoutes.index);
     app.get('/helloworld/welcome', helloworldRoutes.welcome);
+
+    //Movie Routes
     app.get('/movies', moviesRoutes.index);
     app.get('/movie/create', moviesRoutes.create);
     app.post('/movie/create', moviesRoutes.add);
     app.get('/movie/details/:id', moviesRoutes.details);
     app.get('/movie/delete/:id', moviesRoutes.delete);
+    app.get('/movie/edit/:id', moviesRoutes.edit);
 }
